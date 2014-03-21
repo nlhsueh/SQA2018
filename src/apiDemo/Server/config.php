7e50f657679485379161e4293a2e252be21cf5eb
Lother <lotherex@gmail.com>
2014-03-21 14:48:48 +0800

    remove 403 use api error message

--- src/apiDemo/Server/config.php
+++ src/apiDemo/Server/config.php
@@ -13,10 +13,5 @@
         list($usec, $sec) = explode(" ", microtime());
         return round(((float)$usec + (float)$sec)*1000);
     }
-    function page403()
-    {
-        header("HTTP/1.1 403 Forbidden"); 
-        exit();
-    }
 	
 ?>
