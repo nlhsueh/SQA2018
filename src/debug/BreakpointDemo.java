c97ccc2c2ff4d2ba0da7191a6ef3a9788e1696e0
Nien Lin Hsueh <nlhsueh@fcu.edu.tw>
2016-02-17 14:22:14 +0800

    no message

--- src/debug/BreakpointDemo.java
+++ src/debug/BreakpointDemo.java
@@ -1,22 +1,18 @@
 package debug;
 
-/**
- * Use this demo to see how 'conditional breakpoints' work in debug mode
- * 
- * @author nlhsueh
- *
- */
-
 public class BreakpointDemo {
 
 	public static void main(String[] args) {
 		int[] data = { 1, 2, 3, 10, 9, 6, 7 };
+		int a = 1;
+		int b = 2;
+		int c = a + b; // please set breakpoint here
 
-		for (int i = 0; i < data.length-1; i++) {
-			if (data[i] > data[i + 1]) { //please set breakpoint here
-				// do something
+		for (int i = 0; i < data.length - 1; i++) {
+			if (data[i] > data[i + 1]) { // please set breakpoint here
+				data[i] = data[i+1];
+				data[i+1] = data[i];
 			}
 		}
 	}
 }
-
