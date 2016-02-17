9016244109276e2935a97d07679032322fffda07
nlhsueh <nlhsueh@fcu.edu.tw>
2016-02-17 20:48:35 +0800

    no message

--- src/debug/BreakpointDemo.java
+++ src/debug/BreakpointDemo.java
@@ -4,15 +4,42 @@ public class BreakpointDemo {
 
 	public static void main(String[] args) {
 		int[] data = { 1, 2, 3, 10, 9, 6, 7 };
-		int a = 1;
-		int b = 2;
-		int c = a + b; // please set breakpoint here
+		int len = data.length;
+		printArray(data);
 
+		// 產生一個新的 data2 陣列，但部分內容對調
+		int a = (int) (Math.random() * len + 1);
+		int b = (int) (Math.random() * len + 1);
+
+		int[] data2 = exchange(data, a, b);
+
+		printArray(data2);//看看 交換後的狀況 
+		printArray(data); //data 不該有異動
+
+		sort(data);
+		printArray(data);//看看排序是否正確
+	}
+
+	public static int[] exchange(int[] data, int i, int j) {
+		int temp = data[i];
+		data[i] = data[j];
+		data[j] = temp;
+		return data;
+	}
+
+	public static void sort(int[] data) {
 		for (int i = 0; i < data.length - 1; i++) {
-			if (data[i] > data[i + 1]) { // please set breakpoint here
-				data[i] = data[i+1];
-				data[i+1] = data[i];
+			if (data[i] > data[i + 1]) {
+				data[i] = data[i + 1];
+				data[i + 1] = data[i];
 			}
 		}
 	}
+
+	public static void printArray(int[] data) {
+		for (int x : data)
+			System.out.print(x + "\t");
+		System.out.println();
+	}
+
 }
